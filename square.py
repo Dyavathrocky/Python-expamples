21dec6e8190f9ace4ec6575cf904029e0ce58ec0
Dyavathrocky <davathrak@gmail.com>
2017-04-03 11:53:57 -0700

    Add files via upload

---
+++ square.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  3 23:39:11 2017
+
+@author: raju
+"""
+
+def myfun(x):
+    i=1
+    for i in range(10):
+      z=x**i
+      i=+1
+    return z
+    
+    
+     
+myfun(5)
